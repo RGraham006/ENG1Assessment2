efcb704ee6d76c8c02bfd96af41c58997056b583
LabibZabaneh <labibraedzabaneh@gmail.com>
2023-05-02 16:27:11 +0300

    PowerupManager tests

--- core/src/cs/eng1/piazzapanic/powerups/PowerupManager.java
+++ core/src/cs/eng1/piazzapanic/powerups/PowerupManager.java
@@ -8,25 +8,24 @@ import cs.eng1.piazzapanic.chef.ChefManager;
 import cs.eng1.piazzapanic.food.CustomerManager;
 import cs.eng1.piazzapanic.ui.UIOverlay;
 
-
 /**
- * The controller that gives initialises all powerups and handles any powerup applications.
+ * The controller that gives initialises all powerups and handles any powerup
+ * applications.
  */
 public class PowerupManager {
 
     private final String[] powerupTypes = new String[] {
-        "chef_speed_up",
-        "prep_speed_up",
-        "add_rep_point",
-        "double_money",
-        "reset_customer_wait"
+            "chef_speed_up",
+            "prep_speed_up",
+            "add_rep_point",
+            "double_money",
+            "reset_customer_wait"
     };
     private final ArrayList<Powerup> powerups;
     private final UIOverlay overlay;
     private Powerup appliedPowerup = null;
     private final ChefManager chefManager;
     private final CustomerManager customerManager;
-    
 
     public PowerupManager(ChefManager chefManager, CustomerManager customerManager, UIOverlay overlay) {
         // Initialise powerups using given types
@@ -53,7 +52,7 @@ public class PowerupManager {
 
     public void generatePowerup(int number) {
         appliedPowerup = powerups.get(number);
-        appliedPowerup.applyPowerup();  
+        appliedPowerup.applyPowerup();
     }
 
     public void removePowerup() {
@@ -71,5 +70,9 @@ public class PowerupManager {
     public CustomerManager getCustomerManager() {
         return customerManager;
     }
-    
+
+    public Powerup getAppliedPowerup() {
+        return this.appliedPowerup;
+    }
+
 }
--- tests/src/cs/eng1/piazzapanic/tests/PowerupTests.java
+++ tests/src/cs/eng1/piazzapanic/tests/PowerupTests.java
@@ -2,13 +2,15 @@ package cs.eng1.piazzapanic.tests;
 
 import cs.eng1.piazzapanic.powerups.Powerup;
 import cs.eng1.piazzapanic.powerups.PowerupManager;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 @RunWith(GdxTestRunner.class)
 public class PowerupTests {
 
-    public Powerup initialisePowerup(){
+    public Powerup initialisePowerup() {
         return new Powerup(Mockito.mock(String.class), Mockito.mock(PowerupManager.class));
     }
 
