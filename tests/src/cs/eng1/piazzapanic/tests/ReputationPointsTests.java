224d8a2ea0113170a7acecb2621866e7d5b57177
Amu-x <amy1545@outlook.com>
2023-05-02 17:19:34 +0100

    Timer tests

--- tests/src/cs/eng1/piazzapanic/tests/ReputationPointsTests.java
+++ tests/src/cs/eng1/piazzapanic/tests/ReputationPointsTests.java
@@ -2,7 +2,6 @@ package cs.eng1.piazzapanic.tests;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Random;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import cs.eng1.piazzapanic.ui.ReputationPoint;
 import org.junit.Test;
