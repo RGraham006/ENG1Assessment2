c0d5c214bb5615e51de655813dd6ac4f6e242d01
Amu-x <amy1545@outlook.com>
2023-05-02 17:04:15 +0100

    Reputation point tests

--- tests/src/cs/eng1/piazzapanic/tests/ReputationPointsTests.java
+++ tests/src/cs/eng1/piazzapanic/tests/ReputationPointsTests.java
@@ -2,11 +2,13 @@ package cs.eng1.piazzapanic.tests;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Random;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import cs.eng1.piazzapanic.ui.ReputationPoint;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(GdxTestRunner.class)
@@ -19,9 +21,67 @@ public class ReputationPointsTests {
     }
 
     @Test
-    public void test(){
+    public void testPointSub() {
         ReputationPoint reputationPoint = initialiseReputationPoint();
-        assertTrue(true);
+        int initPoints = reputationPoint.getPoints();
+        reputationPoint.subRepPoint();
+        assertEquals(initPoints - 1, reputationPoint.getPoints());
+    }
+
+    @Test
+    public void testPointAdd() {
+        ReputationPoint reputationPoint = initialiseReputationPoint();
+        reputationPoint.subRepPoint();
+        int initPoints = reputationPoint.getPoints();
+        reputationPoint.addRepPoint();
+        assertEquals(initPoints + 1, reputationPoint.getPoints());
+    }
+
+    @Test
+    public void testCannotAddPoint() {
+        ReputationPoint reputationPoint = initialiseReputationPoint();
+        int initPoints = reputationPoint.getPoints();
+        reputationPoint.addRepPoint();;
+        assertEquals(initPoints, reputationPoint.getPoints());
+    }
+
+    @Test
+    public void testCannotSubPoint() {
+        ReputationPoint reputationPoint = initialiseReputationPoint();
+        int initPoints = reputationPoint.getPoints();
+        for (int i = 0; i < initPoints; i++) {
+            reputationPoint.subRepPoint();
+        }
+        int minPoints = reputationPoint.getPoints();
+        assertEquals(0, minPoints);
+        reputationPoint.subRepPoint();
+        assertEquals(minPoints, reputationPoint.getPoints());
+    }
+
+    @Test
+    public void testGameOver() {
+        ReputationPoint reputationPoint = initialiseReputationPoint();
+        int initPoints = reputationPoint.getPoints();
+        for (int i = 0; i < initPoints; i++) {
+            reputationPoint.subRepPoint();
+        }
+        int minPoints = reputationPoint.getPoints();
+        assertEquals(0, minPoints);
+        assertTrue(reputationPoint.isGameOver());
+    }
+
+    @Test
+    public void testInitPointsAtThree() {
+        ReputationPoint reputationPoint = initialiseReputationPoint();
+        assertEquals(3, reputationPoint.getPoints());
+    }
+
+    @Test
+    public void testSetCustomPoints() {
+        ReputationPoint reputationPoint = initialiseReputationPoint();
+        int randomPoints = ((int) Math.random() * 2) + 1;
+        reputationPoint.setCustomPoints(randomPoints);
+        assertEquals(randomPoints, reputationPoint.getPoints());
     }
 
 }
