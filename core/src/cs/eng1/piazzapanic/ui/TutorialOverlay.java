281ca357702c9c081258aa79a825bf7bff8e9bbe
RGraham006 <rcg525@york.ac.uk>
2023-05-03 01:54:21 +0100

    added tutorial screen from main

--- core/src/cs/eng1/piazzapanic/ui/TutorialOverlay.java
+++ core/src/cs/eng1/piazzapanic/ui/TutorialOverlay.java
@@ -1,9 +1,11 @@
 package cs.eng1.piazzapanic.ui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -26,30 +28,33 @@ public class TutorialOverlay {
     bgPixmap.setColor(Color.LIGHT_GRAY);
     bgPixmap.fill();
     TextureRegionDrawable textureRegionDrawableBg = new TextureRegionDrawable(
-        new Texture(bgPixmap));
+            new Texture(bgPixmap));
     table.setBackground(textureRegionDrawableBg);
 
     // Initialise movement instructions label
     LabelStyle labelStyle = new LabelStyle(game.getFontManager().getHeaderFont(), Color.BLACK);
     Label chefMovement = new Label(
-        "Left click on a chef to select them. Then use WASD or the arrow keys to move around.",
-        labelStyle);
+            "Left click on a chef to select them. Then use WASD or the arrow keys to move around.",
+            labelStyle);
     chefMovement.setWrap(true);
 
     // Initialise station usage label
     Label stationUsage = new Label(
-        "Move towards a station to get possible actions to appear as buttons including placing ingredients, picking up ingredients, and dealing with the ingredients.",
-        labelStyle);
+            "Move towards a station to get possible actions to appear as buttons including placing ingredients, picking up ingredients, and dealing with the ingredients.",
+            labelStyle);
     stationUsage.setWrap(true);
 
     // Initialise recipe creation label
     Label recipeLabel = new Label(
-        "Take the ingredients and process them as required for the recipe shown on the right. Then take them to the counter (table with the orange tablecloth) to build the recipe and complete the order.",
-        labelStyle);
+            "Take the ingredients and process them as required for the recipe shown on the right. Then take them to the counter (table with the orange tablecloth) to build the recipe and complete the order.",
+            labelStyle);
     recipeLabel.setWrap(true);
 
+    Texture recipe = new Texture(Gdx.files.internal("food/recipePage.png"));
+    Image recipePage = new Image(recipe);
+
     TextButton backButton = game.getButtonManager()
-        .createTextButton("Done", ButtonManager.ButtonColour.GREY);
+            .createTextButton("Done", ButtonManager.ButtonColour.GREY);
     backButton.addListener(new ClickListener() {
       @Override
       public void clicked(InputEvent event, float x, float y) {
@@ -63,7 +68,7 @@ public class TutorialOverlay {
     table.row();
     table.add(stationUsage).fillX().expandX().pad(20f).padTop(0f);
     table.row();
-    table.add(recipeLabel).fillX().expandX().pad(20f).padTop(0f);
+    table.add(recipePage).height(200).width(300);
     table.row();
     table.add(backButton).padTop(20f);
   }
